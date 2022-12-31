d94a4fcc49904fb21491bac39e0f90a05cf77ed6
QubitMatrix <preethi13m@gmail.com>
2022-12-31 20:35:42 +0530

    added competitions page

---
+++ src/pages/competitions.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Competitions(){
+    return(<div id="competitions-mid"><h3>RDHub</h3><span>It is a website that can act as a hub for all research based queries. It is quite difficult to find a teammate or mentor for a research idea or to find a topic to start the research process. This website aims to make all this easier by hosting a site which collects all these details from various sources and puts them together in one place.<br/><br/>Kodikon | Research Hub | Web Dev</span></div>)
+}
