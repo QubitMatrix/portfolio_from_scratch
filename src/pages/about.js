70f31fda076ce92478a0bb42a6cab0411ef14891
QubitMatrix <preethi13m@gmail.com>
2022-12-31 13:36:23 +0530

    added about me page

---
+++ src/pages/about.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function About(){
+    return(<div id="about-mid"><img src="" alt="pic"/><br/><span id="about-text"><em>Hey Everyone!!! Preethi. M on this side!</em><br/><br/>2nd year student pursuing B.Tech(CSE) at PES University.<br/>I like building new stuff and solving real world problems and I am open to new opportunities.<br/><br/><u>My interests:</u><br/>Web Development | Mobile App Development | Competitive Coding<br/><br/>Know more about my <a id="git-link" href="https://github.com/QubitMatrix">works</a></span></div>)
+}
