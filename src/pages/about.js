2909eccb083e4de88ccd2266b90017614cb2063e
QubitMatrix <preethi13m@gmail.com>
2023-01-03 11:48:36 +0530

    added image and links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
 import React from "react";
 export default function About(){
-    return(<div id="about-mid"><img src="" alt="pic"/><br/><span id="about-text"><em>Hey Everyone!!! <a href="https://github.com/QubitMatrix" target="_blank">Preethi. M</a> on this side!</em><br/><br/>2nd year student pursuing B.Tech(CSE) at PES University.<br/>I like building new stuff and solving real world problems and I am open to new opportunities.<br/><br/><u>My interests:</u><br/>Web Development | Mobile App Development | Competitive Coding</span></div>)
+    return(<div id="about-mid"><picture><source media="(prefers-color-scheme:dark)" srcSet="home_pic_dark.png"></source><img width="400px" height="300px" src="home_pic.png" alt="main home" id="home-img" /></picture><br/><span id="about-text"><em>Hey Everyone!!! <a href="https://github.com/QubitMatrix" target="_blank">Preethi. M</a> on this side!</em><br/><br/>2nd year student pursuing B.Tech(CSE) at PES University.<br/>I like building new stuff and solving real world problems and I am open to new opportunities.<br/><br/><u>My interests:</u><br/>Web Development | Mobile App Development | Competitive Coding</span></div>)
 }
