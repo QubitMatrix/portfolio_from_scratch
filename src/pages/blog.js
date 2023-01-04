714f762b3c13af0e93f9deb4e9bb1b9dc2b5ab8a
QubitMatrix <preethi13m@gmail.com>
2023-01-05 00:25:40 +0530

    main blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Blog(){
+    
+    return(<div id="blog-mid">
+        <h3><Link to="/blog/ds_blog">Data Structures for Beginners(A complete guide):</Link></h3></div>)
+}
