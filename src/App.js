150a3c485d56c09a748093cd58a232c2717226b1
QubitMatrix <preethi13m@gmail.com>
2023-01-07 16:58:04 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import { HashRouter as Router} from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import About from './pages/about'
 import Skills from './pages/skills'
