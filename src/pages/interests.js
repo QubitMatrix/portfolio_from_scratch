2298d7dbfe9ec54d0b35c41eb2a388f837221a5e
QubitMatrix <preethi13m@gmail.com>
2023-10-16 23:03:32 +0530

    updated interests page

--- src/pages/interests.js
+++ src/pages/interests.js
@@ -1,4 +1,22 @@
 import React from "react";
-export default function Interests(){
-    return(<div id="interests-mid"><div><u><h3>Hobbies:</h3></u>In my spare time I like to play the keyboard and solve puzzles. I occasionally do some artworks and read books.<br/><br/><h3><u>Interests:</u></h3>I find many CS domains intriguing, but these have the most influence on me:<br/><ul><li>Web Development</li><li>Mobile App Development</li><li>Data Structures and Algorithms</li><li>Competitive Coding</li><li>Database Management</li><li>Computer Networking</li></ul></div></div>)
+export default function Interests()
+{
+    return(
+        <div id="interests-mid">
+            <div>
+                <u><h3>Hobbies:</h3></u>
+                <p>In my spare time I like to play the keyboard and solve puzzles. I occasionally do some artworks and read books.</p><br/>
+                <h3><u>Interests:</u></h3>
+                <p>I find many CS domains intriguing, but these have the most influence on me:</p>
+                <ul>
+                    <li>Web Development</li>
+                    <li>Computer Networks</li>
+                    <li>Cybersecurity</li>
+                    <li>Data Structures and Algorithms</li>
+                    <li>Competitive Coding</li>
+                    <li>Database Management</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
