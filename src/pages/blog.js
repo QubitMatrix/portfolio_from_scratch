68f9564560fad30328c226dfb55dca33aa548912
QubitMatrix <preethi13m@gmail.com>
2023-10-16 23:05:18 +0530

    formatted codes

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Blog(){
-    
-    return(<div id="blog-mid">
-        <h3><Link to="/blog/ds_blog">Data Structures for Beginners(A complete guide)</Link></h3>
-        <br/>
-        <h3><Link to="/blog/arp_blog">ARP Spoofing using Python</Link></h3></div>)
+export default function Blog()
+{    
+    return(
+        <div id="blog-mid">
+            <h3><Link to="/blog/ds_blog">Data Structures for Beginners(A complete guide)</Link></h3>
+            <br/>
+            <h3><Link to="/blog/arp_blog">ARP Spoofing using Python</Link></h3>
+        </div>
+    )
 }
